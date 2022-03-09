874bee6683e3f45453448b761dd62717b56e3d3b
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2022-03-08 23:09:59 -0500

    added correct indentation

--- BoardFoot.java
+++ BoardFoot.java
@@ -9,45 +9,45 @@
 import java.util.Scanner;
 
 class BoardFoot {
-    // doing calculations in another function
-    public static float widthCalculations(float userHeightFloat, float userLengthFloat) {
-        float calcWidth = 0;
-        calcWidth = (144 / userLengthFloat)/ userHeightFloat;
-        return calcWidth;
-    }
-    public static void main(String[] args) {
+  // doing calculations in another function
+  public static float widthCalculations(float userHeightFloat, float userLengthFloat) {
+    float calcWidth = 0;
+    calcWidth = (144 / userLengthFloat)/ userHeightFloat;
+    return calcWidth;
+  }
+  public static void main(String[] args) {
 
-      // declaring varaibles
-      float userHeightFloat = 0;
-      float userLengthFloat = 0;
+    // declaring varaibles
+    float userHeightFloat = 0;
+    float userLengthFloat = 0;
         
-        //getting user input
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("How long is your board foot?");
-        String userLength = myObj.nextLine();
-        System.out.println("How tall is your board foot?");
-        String userHeight = myObj.nextLine();
+      //getting user input
+      Scanner myObj = new Scanner(System.in);
+      System.out.println("How long is your board foot?");
+      String userLength = myObj.nextLine();
+      System.out.println("How tall is your board foot?");
+      String userHeight = myObj.nextLine();
         
-        // try catch for both length and height
-      try {
-        // converting user strings to floats
-         userLengthFloat = Float.parseFloat(userLength);
-         userHeightFloat = Float.parseFloat(userHeight);
-      } catch (NumberFormatException e) {
-        userLengthFloat = -1;
-        userHeightFloat = -1;
-      }
-
-      if (userLengthFloat == -1) {
-        System.out.println("input a positive number");
-      } else if (userHeightFloat == -1) {
-        System.out.println("input a positive number");
-      } else {
-        // calling method
-        float width = widthCalculations(userHeightFloat, userLengthFloat);
-
-        // printing user's calculated board foot
-        System.out.println("Your width in cubic inches is " + width);
-      }
+      // try catch for both length and height
+    try {
+      // converting user strings to floats
+      userLengthFloat = Float.parseFloat(userLength);
+      userHeightFloat = Float.parseFloat(userHeight);
+    } catch (NumberFormatException e) {
+      userLengthFloat = -1;
+      userHeightFloat = -1;
     }
+
+    if (userLengthFloat == -1) {
+      System.out.println("input a positive number");
+    } else if (userHeightFloat == -1) {
+      System.out.println("input a positive number");
+    } else {
+      // calling method
+      float width = widthCalculations(userHeightFloat, userLengthFloat);
+
+      // printing user's calculated board foot
+      System.out.println("Your width in cubic inches is " + width);
+    }
+  }
 }
